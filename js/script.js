44d34e90659afd72b8d229a2d1eb0a2eb25aa84f
unknown <harishprp370@gmail.com>
2024-05-20 15:44:00 +0530

    Files updated

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*Header script*/
+
+const exploreBtn = document.querySelector('.explore-btn');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+const dropdownOptions = document.querySelectorAll('.dropdown-option');
+
+exploreBtn.addEventListener('click', () => {
+  dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+});
+
+dropdownOptions.forEach(option => {
+  option.addEventListener('click', () => {
+    const url = option.dataset.url;
+    window.location.href = url;
+  });
+});
+function redirectToSignIn() {
+  window.location.href = 'signin.html';
+}
+
+/*Contact Page*/
